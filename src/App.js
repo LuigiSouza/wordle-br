de3f2f1172c7c1bf3e3e653ad039dd6db028c47f
LuigiSouza <lpsouza@inf.ufsm.br>
2022-01-29 13:26:34 -0300

    feat: responsive board to screen size

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import "./App.css";
 
 import Card from "./components/Card";
 import Grid from "./components/Grid";
+import Board from "./components/Board";
 
 function App() {
+  const words = ["luigi", "teste", "audio", "asdfg", "      "];
+
   return (
     <div className="App">
-      <Grid>
-        <Card>l</Card>
-        <Card>u</Card>
-        <Card>i</Card>
-        <Card>g</Card>
-        <Card>i</Card>
-      </Grid>
+      <header>
+        <span>(NOT) TERMO</span>
+      </header>
+      <div className="main-game">
+        <Board>
+          {words.map((word, index) => (
+            <Grid key={index}>
+              {word.split("", 5).map((letter, index) => (
+                <Card key={index}>{letter}</Card>
+              ))}
+            </Grid>
+          ))}
+        </Board>
+      </div>
     </div>
   );
 }
---
+++ src/components/Board/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+function Board({ children }) {
+  return <div className={styles.board}>{children}</div>;
+}
+
+export default Board;
